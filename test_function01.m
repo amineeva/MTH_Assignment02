3181c126ff5d8c1ca4b5ce878405cd4f8b6e8d57
CatMoonster17 <166651944+CatMoonster17@users.noreply.github.com>
2026-09-21 09:59:48 -0400

    Changed from calling a column to a row

--- test_function01.m
+++ test_function01.m
@@ -1,7 +1,7 @@
 function [fval, J] = test_function01(X)
-    x1 = X(:, 1);
-    x2 = X(:, 2);
-    x3 = X(:, 3);
+    x1 = X(1);
+    x2 = X(2);
+    x3 = X(3);
     f1 = x1^2 + x2^2 - 6 - x3^5;
     f2 = x1*x3 + x2 - 12;
     f3 = sin(x1 + x2 + x3);
@@ -9,7 +9,7 @@ function [fval, J] = test_function01(X)
     J = zeros([length(fval), length(X)]);
     for i = 1:3
         for n = 1:3
-            J(i,n) = diff(fval(:, i), X(:, n))
+            J(i,n) = diff(fval(i), X(n))
         end
     end    
 end
