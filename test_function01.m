a6f376decc32e27ab6874f8d1829358f1b6f4aa2
amineeva <145402935+amineeva@users.noreply.github.com>
2026-09-21 07:06:44 -0700

    Mid-testing of function, not done
    maybe needs to be symbolic?

--- test_function01.m
+++ test_function01.m
@@ -1,7 +1,19 @@
-function [fval, J] = test_function01(X)
-    x1 = X(1);
-    x2 = X(2);
-    x3 = X(3);
+%the function name and input/output variable names
+%are just what I chose, you can use whatever names you'd like
+
+X = [1, 5, 10].';
+[fval, J] = testfunction_01(X)
+
+
+% Inputs:
+% X: column vector input
+% Outputs:
+% f_X: output values
+% J: Jacobian
+function [fval, J] = testfunction_01(X)
+    x1 = X(1, :);
+    x2 = X(2, :);
+    x3 = X(3, :);
     f1 = x1^2 + x2^2 - 6 - x3^5;
     f2 = x1*x3 + x2 - 12;
     f3 = sin(x1 + x2 + x3);
@@ -9,7 +21,13 @@ function [fval, J] = test_function01(X)
     J = zeros([length(fval), length(X)]);
     for i = 1:3
         for n = 1:3
-            J(i,n) = diff(fval(i), X(n))
+            disp(i)
+            disp(n)
+            disp(fval(i, :))
+            disp(X(n, :))
+            deriv = diff(fval(i, :), X(n, :))
+            disp(deriv)
+            J(i,n) = diff(fval(i, :), X(n, :));
         end
     end    
 end
